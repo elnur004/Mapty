a8bcff76a806c06b92572d47589e6adec1e9af02
elnur004 <lnur004@gmail.com>
2021-01-27 04:30:25 +0400

    upd

--- script.js
+++ script.js
@@ -23,31 +23,6 @@ class App {
       navigator.geolocation.getCurrentPosition(this._loadMap, function () {
         alert("Couldn't get your position");
       });
-
-    form.addEventListener('submit', function (e) {
-      e.preventDefault();
-
-      // Clear input fields
-      inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
-        '';
-
-      // Display marker
-      const { lat, lng } = mapEvent.latlng;
-
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .setPopupContent('Workout')
-        .openPopup();
-    });
   }
 
   _loadMap(position) {
@@ -79,6 +54,31 @@ class App {
 }
 
 const app = new App();
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Clear input fields
+  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
+    '';
+
+  // Display marker
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
 
 inputType.addEventListener('change', function () {
   inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
